c7423d836c5beb541a773b1f35c9a08bbd354f42
felipealp <felipealvesalp@gmail.com>
2019-04-01 21:30:37 -0300

    Biblioteca PDF Laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,32 +7,19 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-        //
+        //Commands\Inspire::class,
+        Commands\RelatoriosCron::class,
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
+    
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        //$schedule->command('inspire')->hourly();
+        $schedule->command('relatorios:cron')->monthly();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
+    
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
